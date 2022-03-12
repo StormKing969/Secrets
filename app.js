8c523049eac97e6fe75940727deaffd11db462dc
StormKing969 <wijesinghesaj@gmail.com>
2022-03-12 11:03:29 -0500

    level 2 encryption

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const ejs = require("ejs");
 const mongoose = require ("mongoose");
 const port = process.env.PORT || 3000;
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,10 +20,14 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 ////////////////////// Schema  Section Start //////////////////////
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret."
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
